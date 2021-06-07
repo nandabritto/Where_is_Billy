eb8cb72ebf073f790601f1883673a904348e40f9
nandabritto <nanda.britto@gmail.com>
2021-06-07 20:55:25 +0000

    Add initial js with test questions

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const question = document.querySelector("#question");
+const choices = document.querySelector('.choice-text');
+
+let questions = [{
+        question: 'test1',
+        choice1: '2',
+        choice2: '6',
+        choice3: '8',
+        choice4: '9',
+        answer: 6,
+    },
+    {
+        question: 'test2',
+        choice1: 'oi',
+        choice2: 'tudo',
+        choice3: 'bem',
+        choice4: 'contigo',
+        answer: oi,
+    }
+]
+
