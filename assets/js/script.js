eee0816b51d9bd410e279d84606e6504f338bcf1
nandabritto <nanda.britto@gmail.com>
2021-06-09 20:33:54 +0000

    Add show showQuestion and nextQuestion functions on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,46 @@ const questionElement = document.getElementById('question');
 const questionContainer = document.getElementById('quiz');
 const choiceButtons = document.getElementById('choice-container');
 
-startButton.addEventListener('click', startQuiz )
+startButton.addEventListener('click', startQuiz)
+
+// When executed hide Start Button, shows Question container and Shuffle questions. 
 
 function startQuiz() {
     startButton.classList.add('hide')
-    shuffleQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
+    shuffleQuestions = questionBank.sort(() => Math.random() - .5)
+    currentQuestion = 0
+    nextQuestion()
+}
+
+// initialize page for new question.
+function nextQuestion() {
+//    resetState()
+    showQuestion(shuffleQuestions[currentQuestion])
+}
+
+//Receive question with answers and outputs buttons for each answer
+function showQuestion(pQuestion) {
+    questionElement.innerText = pQuestion.question
+    pQuestion.answers.forEach(answer => {
+        const answerButton = document.createElement('button')
+        answerButton.innerText = answer.text
+        answerButton.classList.add('button')
+        choiceButtons.appendChild(answerButton)
+    })
 }
 
 
-
-
-
-
-
-let questions = [{
-        question: 'test1',
-        choice1: '2',
-        choice2: '6',
-        choice3: '8',
-        choice4: '9',
-        answer: 6,
-    },
-    {
-        question: 'test2',
-        choice1: 'oi',
-        choice2: 'tudo',
-        choice3: 'bem',
-        choice4: 'contigo',
-        answer: oi,
-    }
-]
-
+// Question Bank
+let questionBank = [{
+    question: 'What is 2 + 2?',
+    answers: [{
+            text: '4',
+            correct: true
+        },
+        {
+            text: '22',
+            correct: false
+        }
+    ]
+}]
