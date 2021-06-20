7f6cca167e151e548c1de543acf042e7db6fae30
nandabritto <nanda.britto@gmail.com>
2021-06-20 19:35:09 +0000

    add new questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,6 @@ const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion;
 let shuffleQuestions;
 
-
-const MAX_QUESTIONS = 10;
-
 let scorePoints = 0;
 window.onload = function beginGame() {
     startQuiz()
@@ -25,21 +22,22 @@ nextButton.addEventListener('click', () => {
 });
 
 // When executed hide Start Button, shows Question container and Shuffle questions. 
-
 function startQuiz() {
     shuffleQuestions = questionBank.sort(() => Math.random() - 0.5);
     currentQuestion = 0;
     nextQuestion();
 }
 
+const MAX_QUESTIONS = 10;
+
 // initialize page for new question.
 function nextQuestion() {
-    if (shuffleQuestions.length >= currentQuestion + 1) {
+    if (shuffleQuestions.length >= currentQuestion + 1)  {
         resetQuestion();
         showQuestion(shuffleQuestions[currentQuestion]);
         myTimer();
     }
-    if ((shuffleQuestions.length == currentQuestion)) {
+    if ((shuffleQuestions.length == currentQuestion) || currentQuestion >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", (scorePoints * 100));
         window.location.assign('/end.html');
     }
@@ -93,7 +91,7 @@ function selectedAnswer(a) {
         button.disabled = true;
     })
 }
-
+// Show correct answer after wrong click
 function wrongAnswer() {
     var modal = document.getElementById('myModal');
     var span = document.getElementsByClassName('close')[0];
@@ -106,10 +104,10 @@ function wrongAnswer() {
         if (event.target == modal) {
             modal.style.display = "none"
         }
-           }
+    }
 }
 
-
+// Add contdown function 
 let timer;
 
 function myTimer() {
@@ -126,82 +124,136 @@ function myTimer() {
             Array.from(choiceButtons.children).forEach(button => {
                 button.disabled = true;
             })
-
         }
     }, 1000)
 }
 
-// function myTimer() {
-//     var sec = 10;
-//     var time = setInterval(myTimer, 1000);
-//     document.getElementById('timer').innerHTML = '<i class="far fa-clock"></i>' + ' ' + sec + ' ' + "sec left";
-//     sec--;
-//     if (sec == -1) {
-//         clearInterval(time);
-//         alert("Time out!! :(");
-//         sec = 10;
-//         time = setInterval(myTimer, 1000);
-//     }
-// }
-
 // Question Bank
 const questionBank = [{
-        question: 'Where is Cristo Redentor?',
-        answers: [{
-                text: 'Rio de Janeiro',
-                correct: true
-            },
-            {
-                text: 'Sao Paulo',
-                correct: false
-            }
-        ],
-        correctText: 'teste teste teste test teste',
-        image: 'assets/images/sp.jpg'
-    },
+    question: "I'm going to the country that has the longest coastline in the world",
+    answers: [{
+            text: 'Canada',
+            correct: true
+        },
+        {
+            text: 'Brazil',
+            correct: false
+        },
+        {
+            text: 'USA',
+            correct: false
+        },{
+            text: 'Australia',
+            correct: false
+        }
+    ],
+    correctText: "Canada's coastline is the world's longest, measuring 243,042 km (includes the mainland coast and the coasts of offshore islands).",
+    image: ''
+},
     {
-        question: 'Where can I find Torre de Belem?',
+        question: "I'll visit a famous volcano that detroyed 5 cities in AD 79 and until nowadays is one of the most dangerous volcanos in the world",
         answers: [{
-                text: 'Lisbon',
+                text: 'Mount Samalas - Indonesia',
+                correct: false
+            },
+            {
+                text: 'Mount Vesuvius - Italy',
                 correct: true
             },
-            {
-                text: 'Porto',
+			{
+                text: 'Mount Unzen - Japan',
                 correct: false
-            },
-            {
-                text: 'London',
-                correct: false
-            },
-            {
-                text: 'Dublin',
+            },{
+                text: 'Santorini - Greece',
                 correct: false
             }
         ],
-        correctText: 'teste teste teste test teste',
-        image: 'assets/images/teste.jpg'
+        correctText: 'The eruption of Mount Vesuvius in AD 79 destroyed the Roman cities of Pompeii, Herculaneum, Oplontis and Stabiae, as well as several other settlements. Today, it is regarded as one of the most dangerous volcanoes in the world because of the population of 3,000,000 people living near enough to be affected by an eruption, with 600,000 in the danger zone, making it the most densely populated volcanic region in the world',
+        image: ''
     },
 
     {
-        question: 'Where can I find Spire?',
+        question: "Dracula famously lived in the historical region of Transylvania – but in what country would I'll find his castle?",
         answers: [{
-                text: 'Lisbon',
+                text: 'Hungary',
                 correct: false
             },
             {
-                text: 'Porto',
+                text: 'Romania',
+                correct: true
+            },
+			{
+                text: 'Slovakia',
+                correct: false
+            },{
+                text: 'Czech Republic',
+                correct: false
+            }
+        ],
+        correctText: "Bran Castle is a castle in Bran.  It is a national monument and landmark in Transylvania. Commonly known outside Transylvania as Dracula's Castle, it is often referred to as the home of the title character in Bram Stoker's Dracula. There is no evidence that Stoker knew anything about this castle, which has only tangential associations with Vlad the Impaler, voivode of Wallachia, the putative inspiration for Dracula. Stoker's description of Dracula's crumbling fictional castle also bears no resemblance to Bran Castle.",
+        image: 'assets/images/bran-castle.jpg'
+    },
+
+{
+    question: "I'll finally visit the city where Antonio Gaudi's Holy Family Basilica is located!",
+        answers: [{
+                text: 'Madrid',
                 correct: false
             },
             {
-                text: 'London',
+                text: 'Sevilha',
                 correct: false
             },
-            {
-                text: 'Dublin',
+			{
+                text: 'Bilbau',
+                correct: false
+            },{
+                text: 'Barcelona',
                 correct: true
             }
         ],
-        correctText: 'teste teste teste test teste',
-        image: 'assets/images/teste.jpg'
-    }
+        correctText: "The Basílica de la Sagrada Família ('Basilica of the Holy Family'), also known as the Sagrada Família, is a large unfinished Roman Catholic minor basilica in the Eixample district of Barcelona, Catalonia, Spain. Designed by the Spanish architect Antoni Gaudí (1852–1926), his work on the building is part of a UNESCO World Heritage Site.",
+        image: 'assets/images/holy-family.jpg'
+    },
+    {question: "I'm headed to the African country that has the largest population! ",
+    answers: [{
+            text: 'South Africa',
+            correct: false
+        },
+        {
+            text: 'Egypt',
+            correct: false
+        },
+        {
+            text: 'Nigeria',
+            correct: true
+        },{
+            text: 'Angola',
+            correct: false
+        }
+    ],
+    correctText: "As the most populous nation in Africa, Nigeria is home to over 206 million inhabitants. Each year, its population increases by nearly 5.5 million. With 2.64% of the world’s population, Nigeria is the seventh most populous country worldwide.",
+    image: 'assets/images/nigeria.jpg'
+},
+{
+    question: "I'm about to go to Maho Beach, famous for its proximity to the island's international airport. Where am I going to?",
+        answers: [{
+                text: 'San Martin',
+                correct: true
+            },
+            {
+                text: 'Bahamas',
+                correct: false
+            },
+			{
+                text: 'Aruba',
+                correct: true
+            },{
+                text: 'Dominican Republic',
+                correct: false
+            }
+        ],
+        correctText: "Due to the unique proximity of low-flying airliners arriving and departing from Princess Juliana International Airport, the location is popular with plane spotters. This is one of the few places in the world where aircraft can be viewed in their flight path just outside the end of the runway. Watching airliners pass over the beach is such a popular activity that daily arrivals and departures airline timetables are displayed on a board in most bars and restaurants on the beach.",
+        image: 'assets/images/maho-beach.jpg'
+}
 ];
